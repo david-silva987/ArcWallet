9719e532bfffab5c4e401aa603a91be599954337
David da Silva <david.dasilva@he-arc.ch>
2020-04-13 18:09:55 +0200

    add modifiy page form

---
+++ ArcWallet/ArcWallet/ModifierMouvement.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ArcWallet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModifierMouvement : ContentPage
+    {
+        object mouvement = null;
+        public ModifierMouvement()
+        {
+            InitializeComponent();
+        }
+
+        public void mouvementEntry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            var picker = sender as Picker;
+            if (picker.Items[picker.SelectedIndex].Equals("Dépense"))
+            {
+                categoryEntry.IsVisible = true;
+                labelCategory.IsVisible = true;
+            }
+            else
+            {
+                categoryEntry.IsVisible = false;
+                labelCategory.IsVisible = false;
+            }
+        }
+
+        public ModifierMouvement(object mouvement,int flag) //flag : 1->depense,2->revenu
+        {
+            if(flag==1)
+            {
+                this.mouvement = mouvement;
+                Console.WriteLine();
+
+            }
+            InitializeComponent();
+        }
+
+        async void modifyMouvementButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ArcWallet/ArcWallet/Toast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcWallet
+{
+    interface Toast
+    {
+        void Show(string message);
+
+    }
+}
