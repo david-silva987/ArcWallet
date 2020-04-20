d2de622ff5483adae67c8e27c1584240ae4c2659
David da Silva <david.dasilva@he-arc.ch>
2020-04-20 18:30:46 +0200

    fix bug with graph

--- ArcWallet/ArcWallet/PageGraphes.xaml.cs
+++ ArcWallet/ArcWallet/PageGraphes.xaml.cs
@@ -34,7 +34,13 @@ namespace ArcWallet
                 Chart1.IsVisible = true;
                 labelNoActivity.IsVisible = false;
             }
+            else
+            {
 
+                    Chart1.IsVisible = false;
+                    labelNoActivity.IsVisible = true;
+                
+            }
 
             List<Entry> entries = new List<Entry>()
             {
