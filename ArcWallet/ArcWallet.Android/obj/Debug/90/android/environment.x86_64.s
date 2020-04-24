5a67e509fbad8d1f6a14f99fa3b00f1a0597dd12
David da Silva <david.dasilva@he-arc.ch>
2020-04-24 13:45:11 +0200

    fix design

--- ArcWallet/ArcWallet.Android/obj/Debug/90/android/environment.x86_64.s
+++ ArcWallet/ArcWallet.Android/obj/Debug/90/android/environment.x86_64.s
@@ -69,7 +69,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"bc81f735-6116-4c78-9dd7-5959dd201a93"
+	.asciz	"4ed51027-8475-418b-b508-89f8aba85a97"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
